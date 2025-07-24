ca16ab0e6a91bc715eeb2cc58420cd91aa21f64b
NodeJS <kashids961@gmail.com>
2025-07-24 11:01:55 +0530

    app

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@ let bodyParser = require("body-parser")
 require("dotenv").config()
 let router = require("../src/routers/router.js")
 let db = require("../db.js")
-
 let app = express()
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 app.use("/",router)
-
 module.exports = app
