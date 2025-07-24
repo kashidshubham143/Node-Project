884043c933720ae73013d3abc95e787b70873051
NodeJS <kashids961@gmail.com>
2025-07-24 11:24:41 +0530

    index

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 let mysql = require("mysql2")
 require("dotenv").config()
-
 let conn = mysql.createConnection({
     host:process.env.db_host,
     user:process.env.db_user,
     password:process.env.db_pass,
     database:process.env.db_database
 })
-
 conn.connect((err) => {
     if(err){
         console.log("Database Not Connected...");
