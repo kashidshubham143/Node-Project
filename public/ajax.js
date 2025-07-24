80d6c85327b766f1d54047930722be7f41d9d567
NodeJS <kashids961@gmail.com>
2025-07-24 11:01:11 +0530

    Public

--- public/ajax.js
+++ public/ajax.js
@@ -1,5 +1,6 @@
 
-let searchProduct = (str) => {
+let searchProduct = (str) => 
+    {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
