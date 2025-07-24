91a93b4c2d6821fb7433216c91a5ceb7a7e79f07
NodeJS <kashids961@gmail.com>
2025-07-24 11:01:38 +0530

    Model

--- src/models/prodModel.js
+++ src/models/prodModel.js
@@ -1,5 +1,4 @@
 let db = require("../../db.js")
-
 exports.newProduct=(name,category,price,quantity)=>{
     return new Promise((resolve,reject)=>{
         db.query("insert into product values ('0',?,?,?,?)",[name,category,price,quantity],(err,result)=>{
